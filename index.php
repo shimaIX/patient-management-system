258216a828f9a099e921cb46147b5949c8d94b8c
Raymund Yanni Hernandez <syefye1316@gmail.com>
2025-04-26 07:44:52 +0800

    created index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MHC Patient Management System</title>
+</head>
+<body>
+    <h1>Mental Health Case Patient Management System</h1>
+    <?php 
+    include 'add-patient.php';
+    echo "<br>";
+    include 'search-patient.php';
+    ?>
+</body>
+</html>
